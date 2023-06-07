8bd24eaa9ec32c8cbbc45733c93f242c5ba32627
Itay <itay@itaysoft.com>
2023-06-07 16:43:40 -0300

    Add C++ version to build script

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ xcodebuild archive \
  -destination 'generic/platform=iOS Simulator' \
  BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
  INSTALL_PATH='Library/Frameworks' \
- SKIP_INSTALL=NO
+ SKIP_INSTALL=NO \
+ CLANG_CXX_LANGUAGE_STANDARD=c++17
 
 xcodebuild archive \
  -scheme ETTrace \
@@ -14,7 +15,8 @@ xcodebuild archive \
  -destination 'generic/platform=iOS' \
  BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
  INSTALL_PATH='Library/Frameworks' \
- SKIP_INSTALL=NO
+ SKIP_INSTALL=NO \
+ CLANG_CXX_LANGUAGE_STANDARD=c++17
 
 xcodebuild -create-xcframework \
  -framework ./ETTrace-iphonesimulator.xcarchive/Products/Library/Frameworks/ETTrace.framework \
