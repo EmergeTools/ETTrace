f80c694602e578b8947c215f8873b00e38e49d1f
noahsmartin <noahm444@gmail.com>
2023-09-25 17:16:09 -0400

    Update package swift (#55)

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/EmergeTools/peertalk.git", branch: "feature/spm"),
+        .package(url: "https://github.com/EmergeTools/peertalk.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/httpswift/swifter.git", from: "1.5.0")
     ],
