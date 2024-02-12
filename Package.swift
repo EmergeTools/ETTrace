42976b473a584f8e3c28fad1c952267ec3657d89
Noah Martin <noahm444@gmail.com>
2024-02-12 13:46:58 -0500

    Add new platforms

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ETTrace",
-    platforms: [.iOS(.v13), .macOS(.v12)],
+    platforms: [.iOS(.v13), .macOS(.v12), .tvOS(.v13), .visionOS(.v1)],
     products: [
         .library(
             name: "ETTrace",
--- build.sh
+++ build.sh
@@ -18,7 +18,29 @@ xcodebuild archive \
  SKIP_INSTALL=NO \
  CLANG_CXX_LANGUAGE_STANDARD=c++17
 
+xcodebuild archive \
+ -scheme ETTrace \
+ -archivePath ./ETTrace-tvOSSimulator.xcarchive \
+ -sdk appletvsimulator \
+ -destination 'generic/platform=tvOS Simulator' \
+ BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+ INSTALL_PATH='Library/Frameworks' \
+ SKIP_INSTALL=NO \
+ CLANG_CXX_LANGUAGE_STANDARD=c++17
+
+xcodebuild archive \
+ -scheme ETTrace \
+ -archivePath ./ETTrace-visionOSSimulator.xcarchive \
+ -sdk xrsimulator \
+ -destination 'generic/platform=visionOS Simulator' \
+ BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+ INSTALL_PATH='Library/Frameworks' \
+ SKIP_INSTALL=NO \
+ CLANG_CXX_LANGUAGE_STANDARD=c++17
+
 xcodebuild -create-xcframework \
  -framework ./ETTrace-iphonesimulator.xcarchive/Products/Library/Frameworks/ETTrace.framework \
  -framework ./ETTrace-iphoneos.xcarchive/Products/Library/Frameworks/ETTrace.framework \
+ -framework ./ETTrace-visionOSSimulator.xcarchive/Products/Library/Frameworks/ETTrace.framework \
+ -framework ./ETTrace-tvOSSimulator.xcarchive/Products/Library/Frameworks/ETTrace.framework \
  -output ./ETTrace.xcframework
